0bcd6ab7b5fb0d74221d50e20e893f4c03b38366
Jonathan Fambro <startek591@gmail.com>
2023-05-16 14:29:53 -0400

    Create Spy Obj

--- spec/test.Spec.js
+++ spec/test.Spec.js
@@ -172,3 +172,21 @@ describe("A spy when created manually", function(){
   });
 });
 
+describe("Multiple spies, when created manually", function() {
+  let tape;
+
+  beforeEach(function() {
+    tape = jasmine.createSpyObj('tape',['play','pause','stop','rewind']);
+    tape.play();
+    tape.pause();
+    tape.rewind(0)
+  });
+
+  it("creates spies for each requested function", function() {
+    expect(tape.play).toBeDefined();
+    expect(tape.pause).toBeDefined();
+    expect(tape.stop).toBeDefined();
+    expect(tape.rewind).toBeDefined();
+  });
+});
+
